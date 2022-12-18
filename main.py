8a8b6588a1f17b11c8ae9a572a1fd8358bdacc03
selcukayasli <86985987+selcukayasli@users.noreply.github.com>
2022-12-18 20:20:26 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 sayi1 = int(input('Bir sayı giriniz: '))
 sayi2 = int(input('Bir sayı giriniz: '))
 toplam = sayi1 + sayi2
+fark = sayi1 - sayi2
 print("Toplama:",str(toplam))
+print("Fark:",str(fark))
